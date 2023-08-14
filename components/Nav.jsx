4a30cc371d4bddd2960bdaa1a4b98659f04f0587
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-14 15:40:13 -0400

    add mock loggedIn user and style Create Prompt btn

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -1,24 +1,37 @@
-"use client"
+"use client";
 
-import Link from 'next/link';
-import Image from 'next/image';
-import { useState, useEffect } from 'react';
-import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
+import Link from "next/link";
+import Image from "next/image";
+import { useState, useEffect } from "react";
+import { signIn, signOut, useSession, getProviders } from "next-auth/react";
 
 const Nav = () => {
+  const isUserLoggedIn = true;
   return (
-    <nav className='flex-between w-full mb-16 pt-3'>
-      <Link href="/" className='flex gap-2 flex-center'>
+    <nav className="flex-between w-full mb-16 pt-3">
+      <Link href="/" className="flex gap-2 flex-center">
         <Image
           src="/assets/images/logo.svg"
           alt="Promptopia Logo"
           width={30}
           height={30}
-          className='object-contain'
+          className="object-contain"
         />
+        <p className="logo_text">Promptopia</p>
       </Link>
-    </nav>
-  )
-}
 
-export default Nav
+      {/* Mobile Nav */}
+      <div className="sm:flex hidden">
+        {isUserLoggedIn ? (
+          <div className="flex gap-3 md:gap-5">
+            <Link href="/create-prompt" className="black_btn">Create Post</Link>
+          </div>
+        ) : (
+          <></>
+        )}
+      </div>
+    </nav>
+  );
+};
+
+export default Nav;
