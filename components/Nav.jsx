db43fef6324750b18cd066066cf5cf59350c1c69
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-14 15:42:40 -0400

    add signout button and mock profile image to navbar

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -25,6 +25,18 @@ const Nav = () => {
         {isUserLoggedIn ? (
           <div className="flex gap-3 md:gap-5">
             <Link href="/create-prompt" className="black_btn">Create Post</Link>
+            <button type="button" onClick={signOut} className="outline_button">
+              Sign Out
+            </button>
+            <Link href="/profile">
+              <Image
+                src="/assets/images/logo.svg"
+                width={37}
+                height={37}
+                className="rounded-full"
+                alt="profile"
+              />
+            </Link>
           </div>
         ) : (
           <></>
