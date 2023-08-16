4e2f01966b1503e85c63d754fa3cfcf2a0bb33ff
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-16 09:44:34 -0400

    add session code to check user

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -12,7 +12,13 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {},
+  async session({ session }) {
+    const sessionUser = await User.findOne({
+      email: session.user.email,
+    });
+    session.user.id = sessionUser._id.toString();
+    return session;
+  },
   async signIn({ profile }) {
     try {
       await connectToDb();
