874bc3d5a320f722c58823675f8f9341be2e9642
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-16 12:45:44 -0400

    add create prompt code which sends response body to api

--- app/create-prompt/page.jsx
+++ app/create-prompt/page.jsx
@@ -1,30 +1,48 @@
-"use client"
-import React, { useState } from 'react';
-import { useSession } from 'next-auth/react';
-import { useRouter } from 'next/router';
+"use client";
+import React, { useState } from "react";
+import { useSession } from "next-auth/react";
+import { useRouter } from "next/router";
 
-import Form from '@components/Form';
+import Form from "@components/Form";
 
 const CreatePrompt = () => {
-  const [ submitting, setSubmitting ] = useState(false);
+  const [submitting, setSubmitting] = useState(false);
   const [post, setPost] = useState({
-    prompt: '',
-    tag: '',
+    prompt: "",
+    tag: "",
   });
 
   const createPrompt = async (e) => {
-
+    e.preventDefault();
+    setSubmitting(true);
+    try {
+      const response = await fetch("/api/prompt/new", {
+        method: "POST",
+        body: JSON.stringify({
+          prompt: post.prompt,
+          userId: session?.user.id,
+          tag: post.tag,
+        }),
+      })
+      if(response.ok) {
+        Router.push('/')
+      }
+    } catch (error) {
+      console.log('Error: ', error)
+    } finally {
+      setSubmitting(false);
+    }
   };
 
   return (
-   <Form
-    type="Create"
-    post={post}
-    setPost={setPost}
-    submitting={submitting}
-    handleSubmit={createPrompt}
-   />
-  )
-}
+    <Form
+      type="Create"
+      post={post}
+      setPost={setPost}
+      submitting={submitting}
+      handleSubmit={createPrompt}
+    />
+  );
+};
 
 export default CreatePrompt;
