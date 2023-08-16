d0004d023238044f24073a8a45246db70e021df5
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-16 08:55:55 -0400

    import Provider to layout and wrap everything inside body

--- app/layout.jsx
+++ app/layout.jsx
@@ -1,27 +1,29 @@
-import '@styles/globals.css';
+import "@styles/globals.css";
 
-import Nav from '@components/Nav';
-import Provider from '@components/Provider';
+import Nav from "@components/Nav";
+import Provider from "@components/Provider";
 
 export const metadata = {
   title: "Proptopia",
-  description: "Discover and Share AI Prompts"
+  description: "Discover and Share AI Prompts",
 };
 
 const RootLayout = ({ children }) => {
   return (
     <html lang="en">
       <body>
-        <div className="main">
-          <div className="gradient"/>
-        </div>
-        <main className="app">
-          <Nav />
-          {children}
-        </main>
+        <Provider>
+          <div className="main">
+            <div className="gradient" />
+          </div>
+          <main className="app">
+            <Nav />
+            {children}
+          </main>
+        </Provider>
       </body>
     </html>
-  )
-}
+  );
+};
 
 export default RootLayout;
