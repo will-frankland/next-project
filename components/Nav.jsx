623be56f153469b8071c7cbff3e4c54607e8abad
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-16 11:03:01 -0400

    modify image to use session.user.image

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -46,7 +46,7 @@ const Nav = () => {
             </button>
             <Link href="/profile">
               <Image
-                src="/assets/images/logo.svg"
+                src={session?.user.image}
                 width={37}
                 height={37}
                 className="rounded-full"
@@ -76,7 +76,7 @@ const Nav = () => {
         {session?.user ? (
           <div className="flex">
             <Image
-              src="/assets/images/logo.svg"
+              src={session?.user.image}
               width={37}
               height={37}
               className="rounded-full"
