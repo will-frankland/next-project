a45d839e424903f328e59c3954a829bc5f2d9d77
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-16 08:42:25 -0400

    add useState and useEffect for setting provider and supply to conditional in code (will use for google auth later)

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -4,9 +4,20 @@ import Link from "next/link";
 import Image from "next/image";
 import { useState, useEffect } from "react";
 import { signIn, signOut, useSession, getProviders } from "next-auth/react";
+import { get } from "mongoose";
 
 const Nav = () => {
   const isUserLoggedIn = true;
+  const [providers, setProviders] = useState(null);
+
+  useEffect(() => {
+    const setProviders = async () => {
+      const response = await getProviders();
+
+      setProviders(response);
+    };
+  }, []);
+
   return (
     <nav className="flex-between w-full mb-16 pt-3">
       <Link href="/" className="flex gap-2 flex-center">
@@ -24,7 +35,9 @@ const Nav = () => {
       <div className="sm:flex hidden">
         {isUserLoggedIn ? (
           <div className="flex gap-3 md:gap-5">
-            <Link href="/create-prompt" className="black_btn">Create Post</Link>
+            <Link href="/create-prompt" className="black_btn">
+              Create Post
+            </Link>
             <button type="button" onClick={signOut} className="outline_button">
               Sign Out
             </button>
@@ -39,7 +52,19 @@ const Nav = () => {
             </Link>
           </div>
         ) : (
-          <></>
+          <>
+          {providers &&
+            Object.values(providers).map((provider) => (
+              <button
+                type="button"
+                key={provider.name}
+                onClick={() => signIn(provider.id)}
+                className="black_btn"
+              >
+                Sign In
+              </button>
+            ))}
+          </>
         )}
       </div>
     </nav>
