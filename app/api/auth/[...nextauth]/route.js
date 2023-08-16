c45f09f7781b37a4d46febe6c34ae7500b76cef2
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-16 09:41:45 -0400

    add code to check for user or create new user inside route

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,32 +1,41 @@
 import NextAuth from "next-auth/next";
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider from "next-auth/providers/google";
 
+// Mongo DB
 import { connectToDb } from "@utils/database";
+import User from "@models/user";
 
 const handler = NextAuth({
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    })
+    }),
   ],
-  async session({ session }) {
-
-  },
+  async session({ session }) {},
   async signIn({ profile }) {
     try {
       await connectToDb();
       // check user exists
-
+      const userExists = await User.findOne({
+        email: profile.email,
+      });
 
       // if not, create user
+      if (!userExists) {
+        await User.create({
+          email: profile.email,
+          username: profile.name.replace(" ", "").toLowerCase(),
+          image: profile.picture,
+        });
+      }
 
       return true;
     } catch (error) {
-      console.log('Error: ', error)
+      console.log("Error: ", error);
       return false;
     }
   },
-})
+});
 
-export { handler as GET, handler as POST  };
+export { handler as GET, handler as POST };
