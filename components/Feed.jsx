16bff098c939742ed87471e465debbd263699e3a
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-21 08:44:46 -0400

    create api call for posts and then map inside PromptCardList

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -7,7 +7,13 @@ import PromptCard from './PromptCard';
 const PromptCardList = ({ data, handleTagClick }) => {
   return (
     <div className='mt-16 prompt_layout'>
-
+      {data.map((post) => (
+        <PromptCard
+          key={post._id}
+          post={post}
+          handleTagClick={handleTagClick} 
+        />
+      ))}
     </div>
   )
 };
@@ -15,10 +21,20 @@ const PromptCardList = ({ data, handleTagClick }) => {
 const Feed = () => {
 
   const [searchText, setSearchText] = useState('');
+  const [posts, setPosts] = useState([]);
 
   const handleSearchChange = (e) => {
 
   };
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch('/api/prompt');
+      const data = await response.json();
+      setPosts(data);
+    }
+    fetchPosts();
+  }, []);
 
   return (
     <section className='feed'>
@@ -34,7 +50,7 @@ const Feed = () => {
         </input>
       </form>
       <PromptCardList
-        data={[]}
+        data={posts}
         handleTagClick={() => {}}
       />
     </section>
