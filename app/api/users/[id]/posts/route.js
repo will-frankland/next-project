ec1c54d82fc475cda5a7c794065b72f2819ccaf4
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-21 12:39:22 -0400

    add Profile setup for page to display prompts made by user

--- app/api/users/[id]/posts/route.js
+++ app/api/users/[id]/posts/route.js
@@ -4,15 +4,14 @@ import Prompt from "@models/prompt";
 export const GET = async (request, { params }) => {
   try {
     await connectToDb();
-    params.id;
     const prompts = await Prompt.find({
-      creator: params.id,
+      creator: params.id
     }).populate("creator");
     return new Response(JSON.stringify(prompts), {
       status: 200,
     });
   } catch (error) {
-    return new Response("Failed to fetch all prompts", {
+    return new Response("Failed to fetch user prompts", {
       status: 500,
     });
   }
--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,30 +2,32 @@
 import { useState, useEffect } from "react";
 import { useSession } from "next-auth/react";
 import { useRouter } from "next/navigation";
-import Profile from "@components/profile";
+import Profile from "@components/Profile";
 
 const MyProfile = () => {
   const { data: session } = useSession();
-  const [posts, setPosts] = useState("");
+  const [myPosts, setMyPosts] = useState([]);
+  console.log(session);
 
   useEffect(() => {
     const fetchPosts = async () => {
-      const response = await fetch(`/api/users/${session?.user.id}/ posts`);
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
       const data = await response.json();
-      setPosts(data);
+      console.log('reqData', data)
+      setMyPosts(data);
     };
     if (session?.user.id) fetchPosts();
   }, []);
 
   const handleEdit = () => {};
 
-  const handleDelete = () => {};
+  const handleDelete = async () => {};
 
   return (
     <Profile
       name="My"
       desc="Welcome to your personalized profile page"
-      data={posts}
+      data={myPosts}
       handleEdit={handleEdit}
       handleDelete={handleDelete}
     />
