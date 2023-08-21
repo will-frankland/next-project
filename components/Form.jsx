52dcb516937fc6e6a07243ba26ba912c3b271108
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-21 09:15:07 -0400

    fix typo causing prompts not to pass to db

--- components/Form.jsx
+++ components/Form.jsx
@@ -37,6 +37,7 @@ const Form = ({ type, post, setPost, submitting, handleSubmit }) => {
             value={post.tag}
             onChange={(e) => setPost({ ...post, tag: e.target.value })}
             placeholder="#tag"
+            type="text"
             required
             className="form_input"
           />
