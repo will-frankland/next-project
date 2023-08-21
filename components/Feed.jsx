bfd606b0278a1c3aaffe239a3900d3bf9483c03e
Will Frankland <50025807+will-frankland@users.noreply.github.com>
2023-08-21 08:38:48 -0400

    add useState and useEffect for searchText and add initial input form with styling

--- components/Feed.jsx
+++ components/Feed.jsx
@@ -2,11 +2,33 @@
 
 import React, {useState, useEffect}  from 'react';
 
+import PromptCard from './PromptCard';
+
 const Feed = () => {
+
+  const [searchText, setSearchText] = useState('');
+
+  const handleSearchChange = (e) => {
+
+  };
+
   return (
-    <div>
-      Feed
-    </div>
+    <section className='feed'>
+      <form className='relative w-full flex-center'>
+        <input
+          type='text'
+          placeholder='Search for a tag or username'
+          value={searchText}
+          onChange={handleSearchChange}
+          required
+          className='search_input peer'
+        >
+        
+        </input>
+
+      </form>
+      
+    </section>
   )
 }
 
